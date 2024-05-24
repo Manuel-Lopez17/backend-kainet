5750237bc35aa9a573e1979134696c5c0cc53e6a
Manuel Lopez <manu1475963@gmail.com>
2024-05-24 12:27:13 -0300

    add addUser.js file

---
+++ addUser.js
@@ -0,0 +1,61 @@
+import { createClient } from '@supabase/supabase-js';
+import { v4 as uuidv4 } from 'uuid';
+import 'dotenv/config';
+
+// Configurar Supabase
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+const usuarioExiste = async (mail) => {
+    const { data, error } = await supabase
+        .from('Usuario')
+        .select('id')
+        .eq('email', mail);
+
+    if (error) {
+        throw error;
+    }
+
+    return data.length > 0;
+};
+
+const crearUsuario = async (nombre, email, habilitado) => {
+    const usuarioYaExiste = await usuarioExiste(email);
+
+    if (usuarioYaExiste) {
+        throw new Error('El usuario ya está registrado');
+    }
+
+    const id = uuidv4();
+    const { data, error } = await supabase
+        .from('Usuario')
+        .insert([
+            { id, nombre, email, habilitado }
+        ]).select();
+
+    if (error) {
+        throw error;
+    }
+
+    return data;
+};
+
+// Obtener los argumentos de la línea de comandos
+const args = process.argv.slice(2);
+const [nombre, email, habilitado] = args;
+
+if (!nombre || !email || typeof habilitado === 'undefined') {
+    console.error('Faltan datos o son incorrectos. Uso: node addUser.js <nombre> <email> <habilitado>');
+    process.exit(1);
+}
+
+(async () => {
+    try {
+        const habilitadoBool = habilitado.toLowerCase() === 'true';
+        const newUser = await crearUsuario(nombre, email, habilitadoBool);
+        console.log('Usuario creado:', newUser);
+    } catch (error) {
+        console.error('Error al crear el usuario:', error.message);
+    }
+})();
